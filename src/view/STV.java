043ddc4299f7aba2e4a75876979b93c89f7cb379
Matheus Dussin Bampi <matheusbampi@gmail.com>
2017-05-02 11:49:42 -0700

    Firebase and OAuth 2.0 removed

--- src/view/STV.java
+++ src/view/STV.java
@@ -31,11 +31,6 @@ public class STV extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
-        
-        
-        
-        
-        
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
@@ -47,73 +42,6 @@ public class STV extends Application {
      */
     public static void main(String[] args) {
         launch(args);
-        
-        // FIREBASE CONFIG
-        try{
-            FileInputStream serviceAccount =
-            new FileInputStream("doc/serviceAccountKey.json");
-        
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-          .setCredential(FirebaseCredentials.fromCertificate(serviceAccount))
-          .setDatabaseUrl("https://stock-trail-vancouver-d8d38.firebaseio.com")
-          .build();
-
-        FirebaseApp.initializeApp(options);
-        // END OF FIREBASE CONFIG
-        
-        // OAUTH 2.0
-        // CLIENT ID: 932786064771-1j3pc0ulilatujh6rh156l4s1n1984gc.apps.googleusercontent.com
-        // CLIENT SECRET KEY: wOeyt9JryVc2tS61bEKCqd3F
-        
-        FileInputStream refreshToken = new FileInputStream("path/to/refreshToken.json");
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-          .setCredential(FirebaseCredentials.fromRefreshToken(refreshToken))
-          .setDatabaseUrl("https://stock-trail-vancouver-d8d38.firebaseio.com")
-          .build();
-
-        FirebaseApp.initializeApp(options);
-        
-        //END OF OAUTH 2.0
-        // INITIALIZE APP
-        // Initialize the default app
-        FirebaseApp defaultApp = FirebaseApp.initializeApp(defaultOptions);
-
-        System.out.println(defaultApp.getName());  // "[DEFAULT]"
-
-        // Retrieve services by passing the defaultApp variable...
-        FirebaseAuth defaultAuth = FirebaseAuth.getInstance(defaultApp);
-        FirebaseDatabase defaultDatabase = FirebaseDatabase.getInstance(defaultApp);
-
-        // ... or use the equivalent shorthand notation
-        FirebaseAuth defaultAuth = FirebaseAuth.getInstance();
-        FirebaseDatabase defaultDatabase = FirebaseDatabase.getInstance();
-        // END OF INITIALIZE APP
-        
-        // OAUTH 2.0
-        try {
-            // URL TO USER AUTHORIZATION
-            URL myURL = new URL("https://accounts.google.com/o/oauth2/v2/auth?"
-                    + "scope=email%20profile&"      
-                    + "response_type=code&"
-                    + "state=security_token%3D138r5719ru3e1%26url%3Dhttps://oauth2.example.com/token&"
-                    + "redirect_uri=http://127.0.0.1:9004&" 
-                    + "client_id=client_id");
-            URLConnection myURLConnection = myURL.openConnection();
-            myURLConnection.connect();
-        }catch (MalformedURLException e) { 
-            // new URL() failed
-            // ...
-        } catch (IOException e) {   
-            // openConnection() failed
-            // ...
-        }        
-        // END OF OAUTH 2.0
-        }catch(FileNotFoundException e){
-            
-        }
-        
     }
     
 }
